b7592c5a80aa355cf4508f00c9c77ab6d832e68f
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-08 22:58:17 -0400

    scopeBindings
    Se aprendió su uso para vincular dos parámetros en url y que puede aplicarse a una única ruta como a un grupo

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\CustomClasses\Acciones;
 use App\Models\Color;
 use App\Enums\Category;
 use App\Enums\Color as EnumsColor;
+use App\Models\Post;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +25,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// ------------------- Crea un usuario y un color
 Route::get('/actions', function () {
     $user  = Acciones::nuevoUsuario("Manuel Henriquez", "manuel@mhenriquez.com", "12345678");
     $color = Acciones::nuevoColor(EnumsColor::RED);
@@ -30,7 +33,7 @@ Route::get('/actions', function () {
     return [$user, $color];
 });
 
-
+// ------------------- Obtiene el color, lo castea a Enum y se obtiene su valor en texto y no en hexadecimal
 Route::get('/color', function () {
     $color = Color::first();
 
@@ -38,6 +41,18 @@ Route::get('/color', function () {
 });
 
 // --------------- Restringir el tipo de datos enviados por la URL
-Route::get('category/{category}', function (Category $category) {
+Route::get('/category/{category}', function (Category $category) {
     return $category->value;
+});
+
+// --------------- scopeBindings verifica si el post le pertenece al usuario, en caso contrario retorna 404
+/* Route::get('/users/{user}/posts/{post}', function (User $user, Post $post) {
+    return [$user, $post];
+})->scopeBindings(); */
+
+// --------------- Para que funcione se deben definir las relaciones en los modelos y se puede aplicar a un grupo
+Route::scopeBindings()->group(function () {
+    Route::get('/users/{user}/posts/{post}', function (User $user, Post $post) {
+        return [$user, $post];
+    });
 });
