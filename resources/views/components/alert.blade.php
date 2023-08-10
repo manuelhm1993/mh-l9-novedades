d951fc908660bc6bfb8da9528b69c987fb9713ae
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-10 01:27:09 -0400

    Atributos html
    Para agregar dinámicamente atributos html, a un componente se usa la variable $attributes y el método merge

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,13 @@
-<div class="bg-{{ $color }}-100 border-t border-b border-{{ $color }}-500 text-{{ $color }}-600 px-4 py-3" role="alert">
+<div role="alert" 
+{{ $attributes->merge(['class' => "bg-$color-100 border-t border-b border-$color-500 text-$color-600 px-4 py-3"]) }}>
+{{-- Se usa merge para especificar los valores del atributo en el componente sumados los recibidos --}}
+
     <p class="font-bold">{{ $title }}</p>
     <p class="text-sm">{{ $slot }}</p>
+
+    {{-- $attributes es un array indexado --}}
+    {{-- @dump($attributes) --}}
+
+    {{-- Si se pasan atributos html al componente y no se reciben en el constructor se almacenan en la variable attributes --}}
+    {{-- {{ $attributes }} --}}
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,12 +19,17 @@
         </style>
     </head>
     <body>
+        @php
+            // ---------- Simular recibir un valor de BBDD
+            $color = "indigo";
+        @endphp
+
         <div class="container mx-auto">
             {{-- Llama a un componente simple y pasa un parámetro --}}
             {{-- <x-alert color="red" /> --}}
 
-            {{-- Llama al componente y le manda información a slot --}}
-            <x-alert color="red">
+            {{-- Pasar una variable como parámetro --}}
+            <x-alert :color="$color" class="mb-4">
                 {{-- slot con nombre, sirve para enviar parámetros largos --}}
                 <x-slot:title>
                     Título 1
@@ -41,6 +46,15 @@
                 </x-slot:title>
 
                 Hola mundo
+            </x-alert>
+
+            <x-alert color="red">
+                {{-- slot con nombre, x-sol:nombre-variable --}}
+                <x-slot:title>
+                    Título 3
+                </x-slot:title>
+
+                Hola mundo 2
             </x-alert>
         </div>
     </body>
